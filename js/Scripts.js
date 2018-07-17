d32cc21de2ca83477deb53f4e85cea1b1c051383
praxyfarhana <praxyfarhana@gmail.com>
2018-07-17 09:59:28 +0300

    adding jquery codes

--- js/Scripts.js
+++ js/Scripts.js
@@ -2,3 +2,15 @@ function Contact(first,last){
   this.firstName = first;
   this.lastName = last;
 }
+
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+    var InputtedFirstName = $("input#new-first-name").val();
+    var InputtedLastName = $("input#new-last-name").val();
+    var newContact = new Contact(InputtedFirstName,InputtedLastName);
+    $("#contacts").append("<li span class='contacts'>" + newContact.firstName + "</span></li>");
+    $("Input#new-first-name").val("");
+    $("Input#new-last-name").val("");
+  });
+});
