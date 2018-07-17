8a3d86f675bff46796e268741f15665af2f8950e
praxyfarhana <praxyfarhana@gmail.com>
2018-07-17 11:30:46 +0300

    added a defination and a prototypemethod for fullname

--- js/Scripts.js
+++ js/Scripts.js
@@ -3,7 +3,7 @@ function Contact(first,last){
   this.firstName = first;
   this.lastName = last;
 }
-Contact.prototype.fullname = function() {return this.firstName + " " + this.lastName;
+Contact.prototype.fullName = function() {return this.firstName + " " + this.lastName;
 }
 //front-end
 $(document).ready(function() {
@@ -12,7 +12,7 @@ $(document).ready(function() {
     var InputtedFirstName = $("input#new-first-name").val();
     var InputtedLastName = $("input#new-last-name").val();
     var newContact = new Contact(InputtedFirstName,InputtedLastName);
-    $("ul#contacts").append("<li span class='contacts'>" + newContact.fullName + "</span></li>");
+    $("ul#contacts").append("<li span class='contact'>" + newContact.fullName() + "</span></li>");
       $(".contact").last().click(function() {
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.firstName);
